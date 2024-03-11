4e2df6200a9c38ab57deac5df57a2f6de26947a4
raju8824 <55652441+raju8824@users.noreply.github.com>
2024-03-11 22:15:19 +0530

    create login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
+import Body from "./components/Body";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Body />
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Logo from "../utils/image/logo.png"
+
+export const Login = () => {
+  return (
+    <div>
+        <div>
+            <img className='bg-gradient-to-r from-black absolute w-60 m-4 z-10 p-8' src={Logo} alt="" />
+        </div>
+        <div className="" >
+            <img className="" src={"https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg"} alt="" />
+        </div>
+        <div>Card</div>
+    </div>
+  )
+}
+
+export default Login
+
+/*
+    - Neflix logo
+    - sign card
+        - input for email
+        - input for password
+        - sign button
+        - chekbox botton
+        - need halp anchertag
+        - signup button
+        - learn more...
+    footer
+        - question call
+        - FAQ
+        - help Center
+        - Terms of use 
+        - Privacy
+        - cookies 
+        - information
+*/ 
